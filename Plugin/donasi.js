8902abb8d5a88984bcf74d41681ed2046c77b47c
G-BOT1 <80567305+G-BOT1@users.noreply.github.com>
2021-03-13 15:17:22 +0700

    Add files via upload

---
+++ Plugin/donasi.js
@@ -0,0 +1,11 @@
+let handler = async m => m.reply(`
+╭─「 Donasi • Pulsa 」
+│ • Indosat Ooredoo [085755846460]
+│ • Gopay [,-]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
